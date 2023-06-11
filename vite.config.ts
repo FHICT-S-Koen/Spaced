e868422f170b8ee454fe9b3111593d80921f2ec4
Ekhorn <koen@kschellingerhout.nl>
2023-06-11 23:08:10 +0200

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
+export default defineConfig(async () => ({
   plugins: [solidPlugin()],
   root: 'app',
+  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
+  // prevent vite from obscuring rust errors
+  clearScreen: false,
   server: {
-    port: 3000,
+    port: 1420,
+    strictPort: true,
   },
+  // to make use of `TAURI_DEBUG` and other env variables
+  // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
+  envPrefix: ["VITE_", "TAURI_"],
   build: {
     target: 'esnext',
     outDir: '../dist'
   },
-});
+}));
