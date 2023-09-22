dd1a379e2a5d2bd9a56c02751af1bd784dd2f8bb
Ekhorn <koen@kschellingerhout.nl>
2023-09-22 02:23:31 +0200

    Use postgres

---
+++ README.md
@@ -0,0 +1,30 @@
+# Spaced
+
+### Prequisites
+
+- Docker
+- Rustc & Cargo
+- sqlx-cli
+- Node 18 + NPM
+
+### Usage
+
+```sh
+npm i
+```
+
+```sh
+docker compose up -d
+```
+
+```sh
+npm run tauri dev
+```
+
+**Reset DB**
+
+```sh
+docker compose down
+docker compose up -d
+sqlx migrate run
+```
--- tauri/src/main.rs
+++ tauri/src/main.rs
@@ -3,13 +3,13 @@
 
 use std::env;
 
-use sqlx::{Pool, Sqlite, SqlitePool};
+use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgPool, Pool, Postgres};
 use tauri::State;
-use serde::{Serialize, Deserialize};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-  let conn = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+  let conn = PgPool::connect(&env::var("DATABASE_URL")?).await?;
   tauri::Builder::default()
     .manage(conn)
     .invoke_handler(tauri::generate_handler![select, insert_into, delete])
@@ -21,39 +21,55 @@ async fn main() -> anyhow::Result<()> {
 
 #[derive(sqlx::FromRow, Serialize, Deserialize, Debug)]
 struct Item {
-  id: i64,
-  x: Option<i64>,
-  y: Option<i64>,
+  id: i32,
+  x: Option<i32>,
+  y: Option<i32>,
   text: Option<String>,
 }
 
 #[tauri::command]
-async fn select(pool: State<'_, Pool<Sqlite>>) -> Result<Vec<Item>, ()>  {
-  let rows = sqlx::query_as!(Item, "SELECT * FROM item").fetch_all(&*pool).await.unwrap();
+async fn select(pool: State<'_, Pool<Postgres>>) -> Result<Vec<Item>, ()> {
+  let rows = sqlx::query_as!(Item, "SELECT * FROM item")
+    .fetch_all(&*pool)
+    .await
+    .unwrap();
   Ok(rows)
 }
 
 #[tauri::command]
-async fn insert_into(pool: State<'_, Pool<Sqlite>>, x: i64, y: i64, text: String) -> Result<Item, ()>  {
-  let user = sqlx::query_as!(
+async fn insert_into(
+  pool: State<'_, Pool<Postgres>>,
+  x: i32,
+  y: i32,
+  text: String,
+) -> Result<Item, ()> {
+  let item = sqlx::query_as!(
     Item,
     r#"
     INSERT INTO item ( x, y, text )
-    VALUES (?1, ?2, ?3);
-
-    SELECT * FROM item ORDER BY rowid DESC;
-    "#, x, y, text,
-  ).fetch_one(&*pool).await.unwrap();
-  Ok(user)
+    VALUES ($1, $2, $3) RETURNING id, x, y, text;
+    "#,
+    x,
+    y,
+    text,
+  )
+  .fetch_one(&*pool)
+  .await
+  .unwrap();
+  Ok(item)
 }
 
 #[tauri::command]
-async fn delete(pool: State<'_, Pool<Sqlite>>, id: i64) -> Result<(), ()>  {
+async fn delete(pool: State<'_, Pool<Postgres>>, id: i32) -> Result<(), ()> {
   sqlx::query!(
     r#"
     DELETE FROM item
-    WHERE id = ?1
-    "#, id,
-  ).execute(&*pool).await.unwrap();
+    WHERE id = $1
+    "#,
+    id,
+  )
+  .execute(&*pool)
+  .await
+  .unwrap();
   Ok(())
 }
