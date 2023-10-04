22374d1ed0ac27d63c079d26d67d3dc97e7bae60
Ekhorn <koen@kschellingerhout.nl>
2023-10-04 21:10:58 +0200

    refactor: use jsx for background component

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,32 +1,15 @@
 export function Background() {
-  const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-  svg.classList.add('w-full', 'h-full', 'absolute', 'z-0');
-  const pattern = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'pattern',
+  return (
+    <svg class="absolute z-0 h-full w-full">
+      <pattern
+        id="background"
+        width="13"
+        height="13"
+        patternUnits="userSpaceOnUse"
+      >
+        <circle cx="0.7" cy="0.7" r="0.7" fill="#363636" />
+      </pattern>
+      <rect x="0" y="0" fill="url(#background)" class="h-full w-full" />
+    </svg>
   );
-  pattern.id = 'he832y89';
-  pattern.setAttribute('patternUnits', 'userSpaceOnUse');
-  const circle = document.createElementNS(
-    'http://www.w3.org/2000/svg',
-    'circle',
-  );
-  circle.setAttribute('cx', '0.7');
-  circle.setAttribute('cy', '0.7');
-  circle.setAttribute('r', '0.7');
-  circle.style.fill = '#363636';
-  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-  rect.style.width = '100%';
-  rect.style.height = '100%';
-  // The following is required to determine how big the pattern should be so how many dots you will see
-  // The size of these dot should be a repeating pattern > 10 > 11 > 15 > 10 > 13 > 17 > 11 > 15 > 20 > 13 > 17 > 22 > 30 > 40 (full zoom)
-  pattern.setAttribute('height', '13');
-  pattern.setAttribute('width', '13');
-  rect.setAttribute('x', '0');
-  rect.setAttribute('y', '0');
-  rect.setAttribute('fill', 'url(#he832y89)');
-  pattern.append(circle);
-  svg.append(pattern);
-  svg.append(rect);
-  return svg;
 }
