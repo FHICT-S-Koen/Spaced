9851cad0de52b7fb2639e631a4ef538841335f21
Ekhorn <koen@kschellingerhout.nl>
2024-01-08 05:13:01 +0100

    chore: cleanup k8s setup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,6 @@
 #! /bin/bash
-k3d registry delete spaced || true
-k3d cluster delete spaced || true
 
-k3d registry create spaced --port 5001
-k3d cluster create spaced --api-port 6550 --port "8888:80@loadbalancer" \
- --servers 1 --agents 1 --registry-use spaced:5001 \
- --k3s-arg "--disable=traefik@server:0"
-
+# Install postgres
 helm install debug-postgres oci://registry-1.docker.io/bitnamicharts/postgresql \
   --set "global.postgresql.auth.secretKeys.adminPasswordKey=password" \
   --set "global.postgresql.auth.secretKeys.userPasswordKey=password" \
@@ -16,6 +10,7 @@ helm install debug-postgres oci://registry-1.docker.io/bitnamicharts/postgresql 
   --set "global.postgresql.service.ports.postgresql=5432" \
   --set "postgresql.fullnameOverride=debug-postgres"
 
+# Install rabbitmq
 helm install debug-rabbitmq oci://registry-1.docker.io/bitnamicharts/rabbitmq \
   --set replicaCount=1 \
   --set auth.username=admin \
@@ -24,19 +19,20 @@ helm install debug-rabbitmq oci://registry-1.docker.io/bitnamicharts/rabbitmq \
   --set "postgresql.fullnameOverride=debug-rabbitmq"
   # --set podManagementPolicy=Parallel
 
+# Setup ingress controller
 helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
 helm repo update
 helm install ingress-nginx ingress-nginx/ingress-nginx \
   --set controller.ingressClass=nginx \
   --set controller.allowSnippetAnnotations=true
-
 echo "Waiting for Ingress controller to be ready..."
 until kubectl wait --for=condition=ready pod -l app.kubernetes.io/component=controller; do
   sleep 5
 done
 
+# Apply manifests
 kubectl apply -f config/manifests/ingress.yaml
-
-docker push localhost:5001/item-producer
 kubectl apply -f config/manifests/service.yaml
 kubectl apply -f config/manifests/item-producer-deployment.yaml
+
+echo "URL: http://localhost:8888/"
