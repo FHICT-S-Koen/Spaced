9bedbf451efdcd24a4c65279e49d7895beb21d37
Ekhorn <koen@kschellingerhout.nl>
2024-01-08 06:35:04 +0100

    chore: add script to create multiple producers

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Parse command-line arguments
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    -p|--producer)
+      producer="$2"
+      shift
+      ;;
+    *)
+      # echo "Unknown parameter: $1"
+      # exit 1
+      ;;
+  esac
+  shift
+done
+
+# item_producer="${!p}"
+
 cleanup() {
   echo "Cleaning up..."
   pkill -P $$
@@ -7,7 +24,11 @@ cleanup() {
 
 trap 'cleanup' EXIT
 
+echo "${!producer}"
+
 echo "Running services..."
-# RUST_LOG=info cargo run -p item_consumer &
-RUST_LOG=info cargo run -p item_producer &
+# cargo run -p item_consumer &
+  for value in "${!p[@]}"; do
+    cargo run -p item_producer -- --port $value &
+  done
 wait
